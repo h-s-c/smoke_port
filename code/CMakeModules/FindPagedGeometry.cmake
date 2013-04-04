d7a2020d16fc8f334aabf6c45c5c9fe1bec95ef3
hsc <hsc@hsc.hsc>
2013-04-04 14:21:29 +0200

    Fixed debug build

--- code/CMakeModules/FindPagedGeometry.cmake
+++ code/CMakeModules/FindPagedGeometry.cmake
@@ -12,7 +12,16 @@ find_path(PG_INCLUDE_DIR PagedGeometry.h HINTS
     ${CMAKE_SOURCE_DIR}/External/ogre-paged/include/PagedGeometry
     )
 
-find_library(PG_LIBRARY NAMES PagedGeometry libPagedGeometry HINTS 
+find_library(PG_LIBRARY_DEBUG NAMES PagedGeometry_d libPagedGeometry_d HINTS 
+    /usr/lib${LIB_SUFFIX}/ogre
+    /usr/lib${LIB_SUFFIX}/OGRE
+    /usr/lib64/OGRE
+    /usr/local/lib${LIB_SUFFIX}/ogre
+    /usr/local/lib${LIB_SUFFIX}/OGRE
+    ${CMAKE_SOURCE_DIR}/External/ogre-paged/lib
+    )
+    
+find_library(PG_LIBRARY_RELEASE NAMES PagedGeometry libPagedGeometry HINTS 
     /usr/lib${LIB_SUFFIX}/ogre
     /usr/lib${LIB_SUFFIX}/OGRE
     /usr/lib64/OGRE
@@ -21,13 +30,16 @@ find_library(PG_LIBRARY NAMES PagedGeometry libPagedGeometry HINTS
     ${CMAKE_SOURCE_DIR}/External/ogre-paged/lib
     )
 
-set(PG_LIBRARIES ${PG_LIBRARY} )
+set(PG_LIBRARIES 
+	debug ${PG_LIBRARY_DEBUG}
+	optimized ${PG_LIBRARY_RELEASE}
+	)
 set(PG_INCLUDE_DIRS ${PG_INCLUDE_DIR} )
 
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set PG_FOUND to TRUE
 # if all listed variables are TRUE
 find_package_handle_standard_args(PagedGeometry  DEFAULT_MSG
-                                  PG_LIBRARY PG_INCLUDE_DIR)
+                                  PG_LIBRARIES PG_INCLUDE_DIR)
 
-mark_as_advanced(PG_INCLUDE_DIR PG_LIBRARY )
+mark_as_advanced(PG_INCLUDE_DIR PG_LIBRARIES PG_LIBRARY_DEBUG PG_LIBRARY_RELEASE)
