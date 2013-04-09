686537003c2bc0d1c3b84a40f98ecba5ed8d6c63
hsc <hsc@hsc.hsc>
2013-04-09 14:22:50 +0200

    Changed license information

--- code/CMakeModules/FindHAVOK.cmake
+++ code/CMakeModules/FindHAVOK.cmake
@@ -1,4 +1,22 @@
-# public domain
+# Copyright (c) 2004-2008, Adrian Boeing.
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without modification, are permitted 
+# provided that the following conditions are met: Redistributions of source code must retain the 
+# above copyright notice, this list of conditions and the following disclaimer. Redistributions in 
+# binary form must reproduce the above copyright notice, this list of conditions and the following 
+# disclaimer in the documentation and/or other materials provided with the distribution.
+# Neither the name of the Physics Abstraction Layer nor the names of its contributors may be used to 
+# endorse or promote products derived from this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR 
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND 
+# FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER 
+# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT 
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 # Locate Havok
 # This module defines XXX_FOUND, XXX_INCLUDE_DIRS and XXX_LIBRARIES standard variables
@@ -16,38 +34,38 @@ FIND_PATH(HAVOK_INCLUDE_DIR Common/Base/hkBase.h
 mark_as_advanced(HAVOK_INCLUDE_DIR)
 
 SET(HAVOK_LIBS 
-	hctCommon 
-	hctSceneExport 
-	hctSdkUtils 
-	hkaAnimation 
-	hkaInternal 
-	hkaRagdoll 
-	hkBase
-	hkcdCollide
-	hkcdInternal
-	hkCompat
-	hkgBridge
-	hkgCommon
-	hkgDx9s
-	hkgDx11
-	hkGeometryUtilities
-	hkgOglES
-	hkgOglES2
-	hkgOgls
-	hkgSoundCommon
-	hkgSoundXAudio2
-	hkInternal
-	hkpCollide
-	hkpConstraint
-	hkpConstraintSolver
-	hkpDynamics
-	hkpInternal
-	hkpUtilities
-	hkpVehicle
-	hkSceneData
-	hkSerialize
-	hkVisualize
-	)
+    hctCommon 
+    hctSceneExport 
+    hctSdkUtils 
+    hkaAnimation 
+    hkaInternal 
+    hkaRagdoll 
+    hkBase
+    hkcdCollide
+    hkcdInternal
+    hkCompat
+    hkgBridge
+    hkgCommon
+    hkgDx9s
+    hkgDx11
+    hkGeometryUtilities
+    hkgOglES
+    hkgOglES2
+    hkgOgls
+    hkgSoundCommon
+    hkgSoundXAudio2
+    hkInternal
+    hkpCollide
+    hkpConstraint
+    hkpConstraintSolver
+    hkpDynamics
+    hkpInternal
+    hkpUtilities
+    hkpVehicle
+    hkSceneData
+    hkSerialize
+    hkVisualize
+    )
 SET(HAVOK_LIBRARIES)
 
 FOREACH(CUR_LIB ${HAVOK_LIBS})
--- code/Systems/PhysicsBULLET/Task.hpp
+++ code/Systems/PhysicsBULLET/Task.hpp
@@ -1,16 +1,20 @@
-// Copyright © 2008-2009 Intel Corporation
-// All Rights Reserved
-//
-// Permission is granted to use, copy, distribute and prepare derivative works of this
-// software for any purpose and without fee, provided, that the above copyright notice
-// and this statement appear in all copies.  Intel makes no representations about the
-// suitability of this software for any purpose.  THIS SOFTWARE IS PROVIDED "AS IS."
-// INTEL SPECIFICALLY DISCLAIMS ALL WARRANTIES, EXPRESS OR IMPLIED, AND ALL LIABILITY,
-// INCLUDING CONSEQUENTIAL AND OTHER INDIRECT DAMAGES, FOR THE USE OF THIS SOFTWARE,
-// INCLUDING LIABILITY FOR INFRINGEMENT OF ANY PROPRIETARY RIGHTS, AND INCLUDING THE
-// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  Intel does not
-// assume any responsibility for any errors which may appear in this software nor any
-// responsibility to update it.
+/* The MIT License (MIT)
+ * Copyright (c) 2013 Kevin Schmidt
+ *  
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
+ * associated documentation files (the "Software"), to deal in the Software without restriction, 
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, 
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is 
+ * furnished to do so, subject to the following conditions:
+ *  
+ * The above copyright notice and this permission notice shall be included in all copies or 
+ * substantial portions of the Software.
+ *  
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT 
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
 
 #pragma once
 
@@ -118,14 +122,9 @@ private:
 private:
     BulletPhysicsScene*                     m_pScene;
 
-    btDiscreteDynamicsWorld*                       m_pWorld;
+    btDiscreteDynamicsWorld*                m_pWorld;
 
     std::list<BulletPhysicsObject*>         m_ActiveObjects;
-
-    //u32                                     m_cJobs;
-
-    //u32                                     m_PrimaryThreadId;
-    //hkpThreadToken                          m_PrimaryThreadToken;
 
     f32                                     m_DeltaTime;
 };
